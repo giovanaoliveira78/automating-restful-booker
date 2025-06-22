e7309d3d529ef9a033284243aad0df7a77125709
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-06-22 17:12:58 -0300

    add scenario: update a booking

--- cypress/e2e/update-booking.cy.js
+++ cypress/e2e/update-booking.cy.js
@@ -1,15 +1,15 @@
 import { faker } from '@faker-js/faker'
 import { generateBookingDates } from '../support/utils'
 
-describe('Create a booking', () => {
+describe('Update a booking', () => {
   let accessToken
+  let bookingId
 
   beforeEach(() => {
     cy.auth().then((token) => {
       accessToken = token
     })
   })
-
   it('Should create a booking', () => {
     const firstName = faker.person.firstName()
     const lastName = faker.person.lastName()
@@ -41,6 +41,38 @@ describe('Create a booking', () => {
       expect(response.body).to.have.property('booking').and.be.an('object')
       expect(response.body.booking.totalprice).to.be.within(100, 1000)
       expect(response.body.booking).to.have.property('bookingdates').and.be.an('object').and.not.be.empty
+
+      bookingId = response.body.bookingid
+    })
+  })
+  it('Should update a booking', () => {
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    const { checkin, checkout } = generateBookingDates()
+    const totalPrice = faker.number.int({ min: 100, max: 1000 })
+
+    cy.api({
+      method: 'PUT',
+      url: `booking/${bookingId}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Cookie': `token=${accessToken}`
+      },
+      body: {
+        "firstname": firstName,
+        "lastname": lastName,
+        "totalprice": totalPrice,
+        "depositpaid": true,
+        "bookingdates": {
+          checkin,
+          checkout,
+        },
+        "additionalneeds": "Breakfast"
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(200)
     })
   })
 })
