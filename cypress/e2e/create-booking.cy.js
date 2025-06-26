300af60e34be84b58a8542f85356532e87f9603e
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-06-26 15:04:20 -0300

    test: add negative test cases for create-booking

--- cypress/e2e/create-booking.cy.js
+++ cypress/e2e/create-booking.cy.js
@@ -44,3 +44,63 @@ describe('Create a booking', () => {
     })
   })
 })
+
+describe('Create a booking - Negative scenarios', () => {
+  let accessToken
+
+  const firstName = faker.person.firstName()
+  const lastName = faker.person.lastName()
+  const { checkin, checkout } = generateBookingDates()
+  const totalPrice = faker.number.int({ min: 100, max: 1000 })
+
+  beforeEach(() => {
+    cy.auth().then((token) => {
+      accessToken = token
+    })
+  })
+
+  it('Should fail to create a booking when the name is not provided', () => {
+    cy.api({
+      method: 'POST',
+      url: '/booking',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+      },
+      body: {
+        "totalprice": totalPrice,
+        "depositpaid": true,
+        "bookingdates": {
+          checkin,
+          checkout,
+        },
+        "additionalneeds": "Breakfast"
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(500)
+    })
+  })
+
+  it('Should fail to create a booking when the booking dates are not provided', () => {
+    cy.api({
+      method: 'POST',
+      url: '/booking',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+      },
+      body: {
+        "firstname": firstName,
+        "lastname": lastName,
+        "totalprice": totalPrice,
+        "depositpaid": true,
+        "bookingdates": {},
+        "additionalneeds": "Breakfast"
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(500)
+    })
+  })
+})
