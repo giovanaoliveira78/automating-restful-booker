ab030ee201d274f41aad6175c153cdb777ba4681
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-07-02 13:49:04 -0300

    add scenario: delete a booking

---
+++ cypress/e2e/delete-booking.cy.js
@@ -0,0 +1,47 @@
+import { faker } from '@faker-js/faker'
+
+let accessToken
+
+beforeEach(() => {
+  cy.auth().then((token) => {
+    accessToken = token
+  })
+})
+
+describe('Delete a booking', () => {
+  it('should delete a booking', () => {
+    cy.createBooking().then((bookingId) => {
+      cy.api({
+        method: 'DELETE',
+        url: `/booking/${bookingId}`,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Cookie': `token=${accessToken}`
+        },
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(201)
+      })
+    })
+  })
+})
+
+describe('Delete a booking - Negative scenarios', () => {
+  it('should fail to delete a booking when the booking ID is not provided', () => {
+    const invalidId = faker.number.int({ min: 99999, max: 999999 })
+
+    cy.api({
+      method: 'DELETE',
+      url: `/booking/${invalidId}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Cookie': `token=${accessToken}`
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(405)
+    })
+  })
+})
