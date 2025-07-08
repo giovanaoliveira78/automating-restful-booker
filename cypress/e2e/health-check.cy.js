d1a120f7b2d72c2771be8e5dbabed5f571420383
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-07-07 21:00:44 -0300

    add scenario: verify the health of the API

---
+++ cypress/e2e/health-check.cy.js
@@ -0,0 +1,11 @@
+describe('Verify the health of the API', () => {
+  it('should verify the health of the API', () => {
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.config('baseUrl')}/ping`,
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+    })
+  })
+})
